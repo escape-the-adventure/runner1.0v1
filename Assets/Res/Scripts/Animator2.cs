85a8f756f6d805c48a5b02045b93b78eb6c3df7f
pawelteken <pawelteken@gmail.com>
2017-05-27 21:09:32 +0200

    Wylaczenie prostowania po skrecaniu
    Skrypt Animator2 podpiety pod Tima ma parametr forceStraight ktory daje
    nam mozliwosc kontroli nad prostowanie tima. (true / false).

--- Assets/Res/Scripts/Animator2.cs
+++ Assets/Res/Scripts/Animator2.cs
@@ -7,7 +7,10 @@ public class Animator2 : MonoBehaviour {
 	public static Animator2 instance;
 
 	public Animator animator;
+
 	public bool canStart = true;
+	public bool forceStraight = false;
+
 	public Transform tr;
 
 	Vector3 newRot = Vector3.zero;
@@ -79,40 +82,43 @@ public class Animator2 : MonoBehaviour {
 	}
 
 	IEnumerator GetStraight(){
-		GameController.instance.canDie = false;
-		yield return new WaitForSeconds(0.5f);
-		GameController.instance.canDie = true;
-		yield return new WaitForSeconds(0.25f);
-//		yield return new WaitForSeconds(0.75f);
-		if (animator.GetCurrentAnimatorStateInfo(0).IsName("run_cycle"))
-		{
-			Debug.Log("[Animator2] {GetStraight}");
-			if(tr.rotation.eulerAngles.y < 100f && tr.rotation.eulerAngles.y > 80f){
-				newRot = new Vector3(0f, 90f, 0f);
-				Debug.Log("[Animator2] {GetStraight} 90");
-			}
-			else if(tr.rotation.eulerAngles.y < 190f && tr.rotation.eulerAngles.y > 170f){
-				newRot = new Vector3(0f, 180f, 0f);
-				Debug.Log("[Animator2] {GetStraight} 180");
-			}
-			else if(tr.rotation.eulerAngles.y < 280f && tr.rotation.eulerAngles.y > 260f){
-				newRot = new Vector3(0f, 270f, 0f);
-				Debug.Log("[Animator2] {GetStraight} 270");
-			}
-			else if(tr.rotation.eulerAngles.y < 10f || tr.rotation.eulerAngles.y > 350f){
-				newRot = new Vector3(0f, 0f, 0f);
-				Debug.Log("[Animator2] {GetStraight} 0");
-			}
 
-			currentRot = tr.rotation;
-			currentLerp = 0f;
+		if(forceStraight){
+			GameController.instance.canDie = false;
+			yield return new WaitForSeconds(0.5f);
+			GameController.instance.canDie = true;
+			yield return new WaitForSeconds(0.25f);
+	//		yield return new WaitForSeconds(0.75f);
+			if (animator.GetCurrentAnimatorStateInfo(0).IsName("run_cycle"))
+			{
+				Debug.Log("[Animator2] {GetStraight}");
+				if(tr.rotation.eulerAngles.y < 100f && tr.rotation.eulerAngles.y > 80f){
+					newRot = new Vector3(0f, 90f, 0f);
+					Debug.Log("[Animator2] {GetStraight} 90");
+				}
+				else if(tr.rotation.eulerAngles.y < 190f && tr.rotation.eulerAngles.y > 170f){
+					newRot = new Vector3(0f, 180f, 0f);
+					Debug.Log("[Animator2] {GetStraight} 180");
+				}
+				else if(tr.rotation.eulerAngles.y < 280f && tr.rotation.eulerAngles.y > 260f){
+					newRot = new Vector3(0f, 270f, 0f);
+					Debug.Log("[Animator2] {GetStraight} 270");
+				}
+				else if(tr.rotation.eulerAngles.y < 10f || tr.rotation.eulerAngles.y > 350f){
+					newRot = new Vector3(0f, 0f, 0f);
+					Debug.Log("[Animator2] {GetStraight} 0");
+				}
 
-			for(int i=0; i<10; i++){
-				tr.rotation = Quaternion.Lerp(currentRot, Quaternion.Euler(newRot), currentLerp);
-				currentLerp += 0.1f;
-				yield return new WaitForSeconds(0.1f);
+				currentRot = tr.rotation;
+				currentLerp = 0f;
+
+				for(int i=0; i<10; i++){
+					tr.rotation = Quaternion.Lerp(currentRot, Quaternion.Euler(newRot), currentLerp);
+					currentLerp += 0.1f;
+					yield return new WaitForSeconds(0.1f);
+				}
+				tr.rotation = Quaternion.Euler(newRot);
 			}
-			tr.rotation = Quaternion.Euler(newRot);
 		}
 	}
 
