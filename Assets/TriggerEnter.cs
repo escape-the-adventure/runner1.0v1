23ae7745bbb6303d218ad3b9cfe9192d0a5b5784
DESKTOP-J1R9VD1\inzyniery <adrian.slusarek@gmail.com>
2018-02-15 22:29:18 +0100

    add modeled scene

---
+++ Assets/TriggerEnter.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerEnter : MonoBehaviour {
+
+	public GameObject gate;
+
+	private void OnTriggerStay()
+	{
+//		gate.SetActive (false);
+		gate.transform.position += gate.transform.up * Time.deltaTime * 5;
+	}
+
+}
